2b2b58942b67e77eb2b7e4e6614b2932b310e5cb
John Lenz <120522526+jlenz27@users.noreply.github.com>
2023-01-20 09:42:02 -0800

    WIP

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,9 @@ function handleFormSubmission() {
     event.preventDefault();
     const passage = document.getElementById("text-passage").value;
     const word = document.getElementById("word").value;
-    const wordCount = wordCounter(passage);
-    const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
     document.getElementById("total-count").innerText = wordCount;
     document.getElementById("selected-count").innerText = occurrencesOfWord;
-    let boldedPassage = boldPassage(word, passage);
-    if (boldedPassage) {
-      document.querySelector("div#bolded-passage").append(boldedPassage);
-    } else {
-      document.querySelector("div#bolded-passage").innerText = null;
-    }
+    
   }
   
   window.addEventListener("load", function() {
