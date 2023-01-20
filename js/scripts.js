d9ff4d683192b0b284652ec13accbe4ea040c3a4
John Lenz <120522526+jlenz27@users.noreply.github.com>
2023-01-20 10:33:28 -0800

    it will update javascript input

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,11 @@
 function handleFormSubmission() {
     event.preventDefault();
-    const passage = document.getElementById("text-passage").value;
-    const word = document.getElementById("word").value;
-    document.getElementById("total-count").innerText = wordCount;
-    document.getElementById("selected-count").innerText = occurrencesOfWord;
-    
-  }
-  
-  window.addEventListener("load", function() {
-    document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
-  });
+    const firstLength = parseInt(document.querySelector("input#input1").value);
+    document.getElementById("total-count").innerText = firstLength;
+    console.log(firstLength);
+
+}
+
+window.addEventListener("load", function () {
+    document.querySelector("form#input1").addEventListener("submit", handleFormSubmission);
+});
