20972336f80d5d0a4fafa7dca2b942878609b6d5
John Lenz <120522526+jlenz27@users.noreply.github.com>
2023-01-20 11:33:05 -0800

    it will complete test 1

--- js/scripts.js
+++ js/scripts.js
@@ -3,27 +3,30 @@
 window.onload = initialize
 
 function initialize() {
-  let form = document.querySelector("form")
-  form.onsubmit = takeForm
+    let form = document.querySelector("form")
+    form.onsubmit = takeForm
 }
 
 function takeForm(event) {
-  event.preventDefault()
-  let number1= parseInt(document.getElementById("number1").value)
-  let numberArray = makeBeepCalculations(number1)
-  displayArray(numberArray)
+    event.preventDefault()
+    let number1 = parseInt(document.getElementById("number1").value)
+    let numberArray = makeBeepCalculations(number1)
+    displayArray(numberArray)
 }
 
 function makeBeepCalculations(number1) {
+    let numbers = [];
+    for (let i = 0; i <= number1; i++) {
+        numbers.push(i);
+    }
+    return numbers;
 
+}
 
+//User Interface Logic
 
-  //User Interface Logic
-
-  function displayArray(numberArray) {
-    let ulFromArray = makeULFromArray(array)
-    let output = document.getElementById("output")
-    output.innerText = ""
-   output.append(ulFromArray)
-  }
-  
+function displayArray(numberArray) {
+    const output1 = document.createElement("p")
+    document.getElementById("results").append(output1);
+    output1.append(numberArray)
+}
