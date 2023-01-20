b3d4e8e8d46ab4ae50755f461d42c4f5eb0eb283
John Lenz <120522526+jlenz27@users.noreply.github.com>
2023-01-20 14:24:00 -0800

    it will pass test 3

--- js/scripts.js
+++ js/scripts.js
@@ -10,11 +10,11 @@ function initialize() {
 function takeForm(event) {
     event.preventDefault()
     let number1 = parseInt(document.getElementById("number1").value)
-    let numberArray = makeBeepCalculations(number1)
+    let numberArray = makeCalculations(number1)
     displayArray(numberArray)
 }
 
-function makeBeepCalculations(number1) {
+function makeCalculations(number1) {
     let numberOutput = [];
     for (let i = 0; i <= number1; i++) {
        numberOutput.push(i);
@@ -23,13 +23,13 @@ function makeBeepCalculations(number1) {
         if (numberOutput[i].toString().includes(3)) {
           numberOutput[i] = "Won't you be my neighbor?"
         }
-    //     else if (numberOutput[i].toString().includes(2)) {
-    //       numberOutput[i] = "Boop!"
-    //     }
-    //     else if (numberOutput[i].toString().includes(1)) {
-    //       numberOutput[i] = "Beep!"
-    //     };
-    //   };
+        else if (numberOutput[i].toString().includes(2)) {
+          numberOutput[i] = "Boop!"
+        }
+        else if (numberOutput[i].toString().includes(1)) {
+          numberOutput[i] = "Beep!"
+        };
+      };
 
       const finalOutput = numberOutput.join(", ");
 
